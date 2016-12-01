4cbb98a5348cf2898df185506c73a9ba556c1fa5
Sam Oluwalana <soluwalana@gmail.com>
2016-11-30 18:59:49 -0800

    Lesson in structuring web apps

--- javadahut/service.py
+++ javadahut/service.py
@@ -1,16 +1,30 @@
+"""This is a docstring, it describes what this file does."""
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+
+class JSHandler(tornado.web.RequestHandler):
+    """The Handler that will return the javascript for our site."""
+
     def get(self):
-        self.write("Hello, world")
+        self.write(open('myscript.js').read())
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
 
-if __name__ == "__main__":
-    app = make_app()
+class IndexHandler(tornado.web.RequestHandler):
+    """The Index HTML Handler."""
+
+    def get(self):
+        self.write(open('index.html').read())
+
+def main():
+    """Entry point for the file."""
+    app = tornado.web.Application([
+        (r'/', IndexHandler),
+        (r'/myscript\.js', JSHandler),
+    ], debug=True)
     app.listen(8080)
+    print('Starting server with port 8080')
     tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    main()
